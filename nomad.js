6319161fa15ab991a159568ebe553eb604439ef1
Loïs Postula <lois@postu.la>
2023-03-30 17:24:29 +0200

    [enh] Add nomad tls thingy

--- nomad.js
+++ nomad.js
@@ -1,10 +1,29 @@
 import got from "got";
+import https from "https";
+import fs from "fs";
 
 const dispatchJob = async function(name, payload) {
     const nomadHost = process.env.NOMAD_HOST || "http://127.0.0.1"
     const nomadToken = process.env.NOMAD_TOKEN || ""
     const nomadJobId = process.env.NOMAD_JOB_ID || ""
     const nomadNamespace = process.env.NOMAD_NAMESPACE || "default"
+    const nomadCaCert = process.env.NOMAD_CACERT || null
+    const nomadClientCert = process.env.NOMAD_CLIENT_CERT || null
+    const nomadClientKey = process.env.NOMAD_CLIENT_KEY || null
+
+    let httpsAgent = null;
+    if (nomadCaCert || nomadClientCert) {
+        const httpsAgentOptions = {}
+        if (nomadCaCert) {
+            httpsAgentOptions.ca = fs.readFileSync(nomadCaCert)
+        }
+        if (nomadClientCert && nomadClientKey) {
+            httpsAgentOptions.cert = fs.readFileSync(nomadClientCert)
+            httpsAgentOptions.key = fs.readFileSync(nomadClientKey)
+            httpsAgentOptions.passphrase = process.env.NOMAD_CLIENT_KEY_PASSPHRASE || undefined
+        }
+        httpsAgent = new https.Agent(httpsAgentOptions);
+    }
 
     const available_node_classes = ["standard", "gpu"]
     let node_class = "standard"
@@ -24,6 +43,9 @@ const dispatchJob = async function(name, payload) {
     }
     try {
         const data = await got.post(`${nomadHost}/v1/job/${nomadJobId}-${node_class}/dispatch`, {
+            agent: {
+                https: httpsAgent,
+            },
             json: {
                 'Meta': {
                     'GH_REPO_URL': payload.repository.html_url,
